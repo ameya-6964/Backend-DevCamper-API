8c404320dfff39bc7bd97f426df46db09fb97d2d
ameya-6964 <belvalkar7@gmail.com>
2024-07-17 16:27:01 +0530

    Connected To Online MongoDB  Database Using Mongoose

--- 2-project/server.js
+++ 2-project/server.js
@@ -1,25 +1,32 @@
 // All Imports
-require("dotenv").config({path:"./config/.env"})
+require("dotenv").config({ path: "./config/.env" })
 const express = require("express");
 const bootcamps = require("./routes/bootcamps");
-const morgan = require('morgan')
+const morgan = require('morgan');
+const connectDB = require("./config/db");
 
+//Connect To Database
+connectDB();
 
 // Variables
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 // Development Logger Middleware
-if(process.env.NODE_ENV === "DEVELOPMENT"){
+if (process.env.NODE_ENV === "DEVELOPMENT") {
   app.use(morgan("dev"))
 }
 
 // Routes
-app.use("/api/v1/bootcamps",bootcamps)
+app.use("/api/v1/bootcamps", bootcamps)
 
+const server = app.listen(PORT, () => {
+  console.log(`Server Is Running On Port : ${process.env.PORT} In ${process.env.NODE_ENV} Mode`);
+})
 
-app.listen(PORT,()=>{
-    console.log(`Server Is Running On Port : ${process.env.PORT} In ${process.env.NODE_ENV} Mode`);
+// Handle Unhandle Rejection 
 
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error : ${err.message}`);
+  server.close(() => process.exit(1))
 })
